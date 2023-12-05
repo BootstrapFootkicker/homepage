92701ae90435ef3e3a44fb38227507afdb613788
bootstrap <ksealy93@gmail.com>
2023-12-05 14:55:43 -0500

    Initial set up
    Finished setting up environment

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+function sum(a,b)
+{
+    return a+b;
+}
+
+
+module.exports = {sum,};
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+
+
+    devtool: 'inline-source-map',
+
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+
+    resolve: {
+        alias: {
+            config$: './configs/app-config.js',
+            react: './vendor/react-master',
+        },
+        extensions: ['.js', '.jsx'],
+        modules: [
+            'node_modules',
+            'bower_components',
+            'shared',
+            '/shared/vendor/modules',
+        ],
+    },
+};
